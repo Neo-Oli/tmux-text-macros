3c436daa375470879b2503a413da2882dee4e45a
Oliver Schmidhauser <oli@glow.li>
2018-04-23 16:26:28 +0200

    remove typo from emoticon

--- emoticons.sh
+++ emoticons.sh
@@ -3,7 +3,7 @@ emoticons=(
 'ಠ_ಠ: Look of Disaproval [emoticon]'
 '(V)g(;,,;) (V): Why not Zoidberg? [emoticon]'
 'ʕ⁰̈●̫⁰̈ʔ: Bear [emoticon]'
-'(g° ͜ʖ ͡°): Lenny [emoticon]'
+'(° ͜ʖ ͡°): Lenny [emoticon]'
 '(╯°□°）╯︵┻━┻:Table flip [emoticon]'
 '(•___•) [emoticon]'
 )
