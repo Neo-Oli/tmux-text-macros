e72a839059b3c8dcdb374b70a90f83d8a5f2a4a9
Oliver Schmidhauser <oli@glow.li>
2019-06-30 18:51:58 +0200

    fix json request and update emojies

--- update-emojis.sh
+++ update-emojis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "emojis=(" > emojis.sh
-curl "https://raw.githubusercontent.com/amio/emoji.json/master/emoji.json" | jq -r ".[] | \"'\(.char): \(.name) [\(.keywords)]'\"" >> emojis.sh
+curl "https://raw.githubusercontent.com/amio/emoji.json/master/emoji.json" | jq -r ".[] | \"'\(.char): \(.name) [\(.category)]'\"" >> emojis.sh
 echo ")" >> emojis.sh
