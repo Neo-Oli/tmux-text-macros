ef0cf75494df621922b31a67b4cd5b3ba909bc5a
Oliver Schmidhauser <oli@glow.li>
2019-06-30 18:48:51 +0200

    remove invisible characters

--- emoticons.sh
+++ emoticons.sh
@@ -2,7 +2,7 @@ emoticons=(
 '¯\_(ツ)_/¯: Shruggie [emoticon]'
 'ಠ_ಠ: Look of Disaproval [emoticon]'
 '(V)g(;,,;) (V): Why not Zoidberg? [emoticon]'
-'ʕ⁰̈●̫⁰̈ʔ: Bear [emoticon]'
+'ʕ⁰●⁰ʔ: Bear [emoticon]'
 '(° ͜ʖ ͡°): Lenny [emoticon]'
 '(╯°□°）╯︵┻━┻:Table flip [emoticon]'
 '(•___•): [emoticon]'
