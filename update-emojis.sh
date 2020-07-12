b216c34a21b1ee67d0154ca946431d30152dfb67
Oliver Schmidhauser <oli@glow.li>
2020-07-12 02:40:43 +0200

    Replace all but the first occurence of colons
    This way the greedy sed will only grab the emoji but still allow for
    colons to be used inside custom macros. Closes #2.

--- update-emojis.sh
+++ update-emojis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "emojis=(" > emojis.sh
-curl "https://raw.githubusercontent.com/amio/emoji.json/master/emoji.json" | jq -r ".[] | \"'\(.char): \(.name) [\(.category)]'\"" >> emojis.sh
+curl "https://raw.githubusercontent.com/amio/emoji.json/master/emoji.json" | jq -r ".[] | \"'\(.char): \(.name) [\(.category)]'\""|sed -e 's/:/;/g' -e 's/;/:/1' >> emojis.sh
 echo ")" >> emojis.sh
