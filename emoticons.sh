4ce79cdbe37a4b2a8b5b7f546f37b04c2aec7379
glow <oli@glow.li>
2022-04-24 21:40:08 +0200

    run prettier on all files

--- emoticons.sh
+++ emoticons.sh
@@ -1,9 +1,9 @@
 emoticons=(
-'¯\_(ツ)_/¯: Shruggie [emoticon]'
-'ಠ_ಠ: Look of Disaproval [emoticon]'
-'(V)g(;,,;) (V): Why not Zoidberg? [emoticon]'
-'ʕ⁰●⁰ʔ: Bear [emoticon]'
-'(° ͜ʖ ͡°): Lenny [emoticon]'
-'(╯°□°）╯︵┻━┻:Table flip [emoticon]'
-'(•___•): [emoticon]'
+    '¯\_(ツ)_/¯: Shruggie [emoticon]'
+    'ಠ_ಠ: Look of Disaproval [emoticon]'
+    '(V)g(;,,;) (V): Why not Zoidberg? [emoticon]'
+    'ʕ⁰●⁰ʔ: Bear [emoticon]'
+    '(° ͜ʖ ͡°): Lenny [emoticon]'
+    '(╯°□°）╯︵┻━┻:Table flip [emoticon]'
+    '(•___•): [emoticon]'
 )
--- update-nerd-fonts-icons.sh
+++ update-nerd-fonts-icons.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 git clone https://github.com/ryanoasis/nerd-fonts.git || git pull -D nerd-fonts
 source nerd-fonts/bin/scripts/lib/i_all.sh
-
 echo "nerd_fonts_icons=(" > nerd-fonts-icons.sh
 for i in $(typeset -p | grep 'declare -- i_' | grep -o -P '(?<=declare -- i_).*(?=\=)'); do
     name=i_$i
