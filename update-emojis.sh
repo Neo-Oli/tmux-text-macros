ea2b03255894b38d245959759b17d77eacb60018
glow <oli@glow.li>
2022-04-24 22:26:15 +0200

    add emoji in square brackets for all emojis

--- update-emojis.sh
+++ update-emojis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "emojis=(" > emojis.sh
 curl "https://raw.githubusercontent.com/muan/unicode-emoji-json/main/data-by-emoji.json" \
-    | jq -r ". | keys[] as \$k | \"'\(\$k): \(.[\$k] | .name ) [\(.[\$k] | .group)]'\"" \
+    | jq -r ". | keys[] as \$k | \"'\(\$k): \(.[\$k] | .name ) (\(.[\$k] | .group)) [Emoji]'\"" \
     | sed -e 's/:/;/g' -e 's/;/:/1' >> emojis.sh
 echo ")" >> emojis.sh
