2d7a76b85247c935f3abb9873257155dd1abffe7
glow <oli@glow.li>
2024-06-19 16:09:04 +0200

    update emojis and add space to nerd fonts icons

--- update-nerd-fonts-icons.sh
+++ update-nerd-fonts-icons.sh
@@ -4,6 +4,6 @@ source nerd-fonts/bin/scripts/lib/i_all.sh
 echo "nerd_fonts_icons=(" > nerd-fonts-icons.sh
 for i in $(typeset -p | grep 'declare -- i_' | grep -o -P '(?<=declare -- i_).*(?=\=)'); do
     name=i_$i
-    echo "'${!name}: $i [Nerd Fonts Icon]'" >> nerd-fonts-icons.sh
+    echo "'${!name} : $i [Nerd Fonts Icon]'" >> nerd-fonts-icons.sh
 done
 echo ")" >> nerd-fonts-icons.sh
